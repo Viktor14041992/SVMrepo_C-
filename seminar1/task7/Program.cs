4010083a42e5a0ac34be35dce41919adb402e6c9
Viktor Smirnov <sonor50792smirnov@yandex.ru>
2022-09-03 19:43:34 +0300

    task7

---
+++ seminar1/task7/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное
+// число и на выходе показывает последнюю цифруэтого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.WriteLine("Введите трёхзначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number >= 100 && number < 1000)
+{
+    double count = number % 10;
+     Console.WriteLine(count);
+}
+else
+{
+    Console.WriteLine("введено не трёхзначное число ");
+}
