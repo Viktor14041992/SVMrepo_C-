2cf3bbf40a574ba5659f4a4b204deb5bfb66a0a4
Viktor Smirnov <sonor50792smirnov@yandex.ru>
2022-09-03 19:34:08 +0300

    task5

---
+++ seminar1/task5/Program.cs
@@ -0,0 +1,14 @@
+﻿// 5. Напишите программу, которая на вход
+// принимает одно число (N), а на выходе показывает все
+// целые числа в провежутке от - N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4."
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = -number;
+while(count <= number)
+{
+    Console.Write(count + " ");
+    count = count + 1;
+}
