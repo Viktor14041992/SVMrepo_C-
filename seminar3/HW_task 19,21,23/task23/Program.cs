b7d6b0aa7a14d974cd609994e7afab48b08eca6f
Viktor Smirnov <sonor50792smirnov@yandex.ru>
2022-09-08 23:04:52 +0300

    task23

---
+++ seminar3/HW_task 19,21,23/task23/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+
+if (n > 0)
+{
+    while (i < n)
+{
+    i = i + 1;
+    double k = Math.Pow(i, 3);
+{
+    Console.Write(k+ ", ");
+}
+};
+
+}
+else
+{
+    Console.WriteLine("Введите число больше 0");
+}
