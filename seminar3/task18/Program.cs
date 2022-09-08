f473f99703b098d26768f96557d8f6025e305d74
Viktor Smirnov <sonor50792smirnov@yandex.ru>
2022-09-08 20:55:37 +0300

    task18

---
+++ seminar3/task18/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+Console.WriteLine("Введите номер кооржинаьной четверти");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n >0 && n < 5)
+{
+if (n == 1)
+{
+    Console.WriteLine("x > 0 && y > 0");
+}
+
+if (n == 2)
+{
+    Console.WriteLine("x < 0 && y > 0");
+}
+
+if (n == 3)
+{
+    Console.WriteLine("x < 0 && y < 0");
+}
+
+if (n == 4)
+{
+    Console.WriteLine("x > 0 && y < 0");
+}
+}
+else
+{
+    Console.WriteLine("номер четверти не корректен");
+}
