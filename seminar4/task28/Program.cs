5b08b41bf496b29f4a8947096d8cd3420fc2ad65
Viktor Smirnov <sonor50792smirnov@yandex.ru>
2022-09-13 22:58:55 +0300

    task28

---
+++ seminar4/task28/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int SumNumbersFromToA(int a)
+{
+    if (a >= 1) //если а >=1, то работаем дальше
+    {
+        int sum = 1;
+        for (int i=1; i <=a; i++)
+        {
+            sum = sum * i;
+        }
+        return sum;
+    }
+    else
+    {
+        Console.WriteLine("A должна быть не меньше 1!");
+        return -1; //неправильная сумма (код ошибки)
+    }
+}
+int number = ReadNumber("Введите число ");
+int summa = SumNumbersFromToA(number);
+Console.WriteLine($"Произведение элементов от 1 до А = {summa}");
