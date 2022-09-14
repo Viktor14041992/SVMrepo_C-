e697878105212902e3e9eb4289d4ccf33b0c4699
Viktor Smirnov <sonor50792smirnov@yandex.ru>
2022-09-14 22:28:38 +0300

    task29

---
+++ seminar4/HW_task25,27/task29/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран. Данные вводятся с консоли пользователем
+
+
+// Случайная генерация: 
+
+// int[]array = new int[8];
+// Random rand = new Random();
+
+// for(int n = 0; n < array.Length; n++)
+// array[n]=rand.Next(0, 10);
+
+// foreach(int arr in array)
+// {
+//     Console.Write(arr +" ");
+// }
+
+//-----------------------------------------------------------------
+
+// Ввод массива:
+
+Console.WriteLine("Введите длину массива:");
+int lenght = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[lenght];
+
+for(int n=0; n<lenght; n++)
+{
+    Console.WriteLine("Введите эл-ты массива:");
+    array[n] = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write("Введённый массив: ");
+for(int n=0; n<lenght; n++)
+{
+    Console.Write(" " + array[n]);
+}
