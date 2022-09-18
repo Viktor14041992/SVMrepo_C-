0d68fc34de0927a56debfab92b49d342754d46d9
Viktor Smirnov <sonor50792smirnov@yandex.ru>
2022-09-18 23:11:56 +0300

    task37

---
+++ seminar5/task37/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном 
+// массиве. Парой считаем первый и последний элемент, 
+// \второй и предпоследний и т.д. Результат запишите в 
+// новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+int[]arr = {6, 7, 3, 6};
+int []arrnew = new int [(arr.Length+1)/2];
+
+for(int i=0; i < arrnew.Length; i++)
+
+{
+    arrnew[i] = arr[i] * arr[arr.Length - 1 - i];
+}
+
+if(arr.Length % 2 != 0)
+{
+    arrnew[arrnew.Length-1] = arr[arr.Length/2];
+}
+Console.WriteLine(string.Join(", ", arrnew));
