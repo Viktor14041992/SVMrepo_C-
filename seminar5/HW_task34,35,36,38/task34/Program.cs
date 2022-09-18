1c443a8ab8197238f54507415b0264f660e02980
Viktor Smirnov <sonor50792smirnov@yandex.ru>
2022-09-19 00:29:26 +0300

    task34

---
+++ seminar5/HW_task34,35,36,38/task34/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 34: Задайте массив заполненный случайными 
+// положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество 
+// чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+int[]array = new int[6];
+Random rand = new Random();
+
+for(int i = 0; i < array.Length; i++)
+array[i]=rand.Next(100, 1000);
+
+
+foreach(int arr in array)
+{
+    Console.Write(arr +" ");
+}
+
+int n =0;
+for(int i =0; i < array.Length; i++)
+
+if(array[i] % 2 == 0)
+{
+    n = n+1;
+}
+ Console.WriteLine(" ");
+ Console.WriteLine($"Кол-во четных чисел в масииве = {n}");
