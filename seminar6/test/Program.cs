a8184dbfdeb337e450f598e7698e0132ba958076
Viktor Smirnov <sonor50792smirnov@yandex.ru>
2022-09-20 20:25:29 +0300

    test

---
+++ seminar6/test/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+
+int[] GetArray(int size, int leftRange, int rightRange)
+{
+    int[] arr = new int [size];
+    Random rand = new Random();
+    for(int i=0; i<size; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange+1);
+
+    }
+    return arr;
+}
+
+int[] arr = GetArray(5, 0, 10);
+int[] copiedArray = arr;
+arr[0] = -1000;
+
+Console.WriteLine($"Исходный массив = {string.Join(", ", arr)}");
+Console.WriteLine($"Скопированный массив = {string.Join(", ", copiedArray)}");
+
+int a = 1;
+int b = a;
+
+a = 6;
+Console.WriteLine(a);
+Console.WriteLine(b);
