3a34bfe7b1410cea384f433aea49b793684205c7
Viktor Smirnov <sonor50792smirnov@yandex.ru>
2022-09-24 12:40:53 +0300

    test

---
+++ seminar7/task46/test/Program.cs
@@ -0,0 +1,17 @@
+﻿// решение задачи 48 на примере одномерного массива
+
+
+
+int[] GetArray(int size, int leftRange, int rightRange)
+{
+    int[] arr = new int [size];
+    Random rand = new Random();
+    for(int i=0; i<size; i++)
+    {
+        arr[i] = i;
+    }
+    return arr;
+}
+int[] arr = GetArray(12, 9, 9);
+Console.Write("Сгенерирован массив:");
+Console.WriteLine(string.Join(", ", arr));
