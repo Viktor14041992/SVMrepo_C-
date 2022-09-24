b7250f72f43ce023323c7b4541ae396a8b73509e
Viktor Smirnov <sonor50792smirnov@yandex.ru>
2022-09-24 12:16:03 +0300

    task46

---
+++ seminar7/task46/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, 
+//заполненный случайными целыми числами.
+
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GetMatrix(int rowsCount, int columsCount, int leftRange = -10, int rightRange = 10) // -10 и 10 - диапазоны по умолчанию, 
+//они не передаются, если мы не укажем другой диапазон
+{
+    int[,] matrix = new int[rowsCount, columsCount];
+
+    Random rand = new Random();
+
+    for(int i=0; i< matrix.GetLength(0); i++) // цикл, обходящий каждый одномерный массив
+    {
+        for(int j=0; j<matrix.GetLength(1); j++) // цикл, обходящий каждый элемент одномерного массива
+        {
+            matrix[i,j] = rand.Next(leftRange, rightRange + 1);  // каждой ячейке присваиваем рандомное значене
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,]matrix) //метод, позволяющий вывести матрицу на экран
+{
+    for(int i=0; i< matrix.GetLength(0); i++) // цикл, обходящий каждый одномерный массив
+    {
+        for(int j=0; j<matrix.GetLength(1); j++) // цикл, обходящий каждый элемент одномерного массива
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine(); //перенос строки, чтобы вывод был в виде матрицы, а не в виде строки
+    }
+}
+
+int m = ReadNumber("Введите количество строк:");
+int n = ReadNumber("Введите количество столбцов:");
+int[,] matr = GetMatrix(m, n); //вызываем метод для нашей матрицы, задаём матрицу, присваиваем ей вызов нашего метода
+PrintMatrix(matr);
