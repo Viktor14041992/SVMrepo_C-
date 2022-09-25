b35327e1fbcd7005fbfad03d01fb307ec72fb403
Viktor Smirnov <sonor50792smirnov@yandex.ru>
2022-09-25 17:26:38 +0300

    task52

---
+++ seminar7/HW_task47,50,52/task52/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GetMatrix(int rowsCount, int columsCount, int leftRange = 0, int rightRange = 10) // -10 и 10 - диапазоны по умолчанию, 
+//они не передаются, если мы не укажем другой диапазон
+{
+    int[,] matrix = new int[rowsCount, columsCount];
+
+    Random rand = new Random();
+
+    for(int i=0; i< matrix.GetLength(0); i++) // цикл, обходящий каждый одномерный массив
+    {
+        for(int j=0; j<matrix.GetLength(1); j++) // цикл, обходящий каждый элемент одномерного массива
+        {
+            matrix[i,j] = rand.Next(leftRange, rightRange + 1);  // каждой ячейке присваиваем рандомное значене
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,]matrix) //метод, позволяющий вывести матрицу на экран
+{
+    for(int i=0; i< matrix.GetLength(0); i++) // цикл, обходящий каждый одномерный массив
+    {
+        for(int j=0; j<matrix.GetLength(1); j++) // цикл, обходящий каждый элемент одномерного массива
+        {
+            Console.Write($"{matrix[i,j]}\t ");
+        }
+        Console.WriteLine(); //перенос строки, чтобы вывод был в виде матрицы, а не в виде строки
+    }
+}
+
+
+double Average(int[,]matrix)
+{
+    double aver = 0;
+    double sum=0;
+
+    for(int j=0; j< matrix.GetLength(0); j++)
+    {
+        sum=0;
+        aver = 0;
+        for(int i=0; i< matrix.GetLength(1); i++)
+        {
+            sum = sum + matrix[i,j];
+        }
+        aver = sum/matrix.GetLength(1);
+        Console.Write("{0,5:f1} ", aver);
+        
+    }
+    return aver;
+}
+
+
+int m = ReadNumber("Введите количество строк:");
+int n = ReadNumber("Введите количество столбцов:");
+int[,] matr = GetMatrix(m, n); //вызываем метод для нашей матрицы, задаём матрицу, присваиваем ей вызов нашего метода
+PrintMatrix(matr);
+Average(matr);
+
+
+
