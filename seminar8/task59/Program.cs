bd2c6c7761693738debd3cdab5f02f7a04530bd2
Viktor Smirnov <sonor50792smirnov@yandex.ru>
2022-09-27 19:38:12 +0300

    task59

---
+++ seminar8/task59/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задача 59: Задайте двумерный массив из целых чисел. Напишите программу, 
+// которая удалит строку и столбец, на пересечении которых расположен наименьший элемент массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Наименьший элемент - 1, на выходе получим
+// следующий массив:
+// 9 4 2
+// 2 2 6
+// 3 4 7
+
+void GetMatrix(int[,]matrix) 
+{
+    Random rand = new Random();
+
+    for(int i=0; i< matrix.GetLength(0); i++) 
+    {
+        for(int j=0; j<matrix.GetLength(1); j++) 
+        {
+            matrix[i,j] = rand.Next(0, 10);  
+            Console.Write($"{matrix[i,j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void SearchMinElementOfMatrix(int[,] matrix)
+{
+    
+    int min=matrix[0,0];
+    int minRow = 0;
+    int minColumn = 0;
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+    for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+        if(matrix[i,j] < min)  
+        { 
+        min=matrix[i,j];
+        minRow = i;
+        minColumn = j;
+        }
+        }
+    }
+    Console.WriteLine($"Наименьший элемент - {min}  на позиции - [{minRow},{minColumn}]");
+    
+    
+    int[,] newMatrix = new int[matrix.GetLength(0)-1, matrix.GetLength(1)-1];
+    
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        if(i == minRow) continue;
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+        if(j == minColumn) continue;
+        Console.Write($"{matrix[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Write("Введите кол-во строк: ");
+int i = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int j = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[i, j]; 
+Console.WriteLine("Исходная матрица:");
+GetMatrix(matrix);
+Console.WriteLine(" ");
+SearchMinElementOfMatrix(matrix);
+
