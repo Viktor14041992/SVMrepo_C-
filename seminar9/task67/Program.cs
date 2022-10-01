8f62421b496511ae17b493b16af741e434aacdf4
Viktor Smirnov <sonor50792smirnov@yandex.ru>
2022-10-01 14:10:38 +0300

    task67

---
+++ seminar9/task67/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 67: Напишите программу, 
+// которая будет принимать на вход число 
+// и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumDigits(int num)  // метод для подсчета суммы цифр в числе
+{
+    
+    if(num / 10 ==0)
+    {
+        return num;  //выход из рекурсии
+    }
+    else
+    {
+        int sum = 0;
+        sum += SumDigits(num / 10) + num % 10;
+        return sum;
+    }
+}
+
+int number = ReadNumber("Введите N: ");
+int sum = SumDigits(number);
+Console.WriteLine( );
+SumDigits(sum);
+Console.WriteLine(sum);
