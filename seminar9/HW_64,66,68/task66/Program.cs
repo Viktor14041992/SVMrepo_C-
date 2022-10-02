3d21fbdf597134ff5c5f6ba90d9a8afc389a6d54
Viktor Smirnov <sonor50792smirnov@yandex.ru>
2022-10-02 14:13:46 +0300

    task66

---
+++ seminar9/HW_64,66,68/task66/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumMtoN(int m, int n)  // метод для подсчета суммы цифр в числе
+{
+    
+    if(n < m)
+    {
+        return 0;  //выход из рекурсии
+    }
+    else
+    {
+        int sum = SumMtoN(m, n-1)+n;
+        n --;
+        return sum;
+    }  
+}
+
+int numberM = ReadNumber("Введите M: ");
+int numberN = ReadNumber("Введите N: ");
+Console.WriteLine( );
+SumMtoN(numberM, numberN);
+Console.WriteLine(SumMtoN(numberM, numberN));
