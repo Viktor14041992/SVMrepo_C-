fcfa7bb9483d1684af4f01d0524c1241951c66f6
Viktor Smirnov <sonor50792smirnov@yandex.ru>
2022-10-02 12:55:40 +0300

    task69

---
+++ seminar9/task69/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, 
+// и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int PowAtoB(int a, int b)  // метод для подсчета суммы цифр в числе
+{
+    
+    if(b <= 1)
+    {
+        return a;  //выход из рекурсии
+    }
+    else
+    {
+        a = PowAtoB(a, b-1)*a;
+        b --;
+        return a;
+    }  
+}
+
+int numberA = ReadNumber("Введите A: ");
+int numberB = ReadNumber("Введите B: ");
+Console.WriteLine( );
+PowAtoB(numberA, numberB);
+Console.WriteLine(PowAtoB(numberA, numberB));
