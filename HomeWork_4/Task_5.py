e3f6a8302aa074aec7940504d025fe58617d99c8
Viktor Smirnov <sonor50792smirnov@yandex.ru>
2022-12-06 20:56:52 +0300

    task 5 has done

--- HomeWork_4/Task_5.py
+++ HomeWork_4/Task_5.py
@@ -13,23 +13,43 @@ print(sp1)
 print(sp2)
 new_sp1 = []
 new_sp2 = []
-e = '^'
 
-for i in range(0, len(sp1) - 2):
+sp1 = sp1[:-3].split('x^')
 
-    if sp1[i] == str(e):
-        continue
-
-    if sp1[i].isdigit() and sp1[i + 1].isdigit() and sp1[i + 2].isdigit():
-        new_sp1.append(sp1[i] + sp1[i + 1] + sp1[i + 2])
-        continue
-    if sp1[i].isdigit() and sp1[i + 1].isdigit():
-        new_sp1.append(sp1[i] + sp1[i + 1])
-        continue
-    if sp1[i].isdigit():
-        new_sp1.append(sp1[i])
-
-    i += 1
+for i in sp1:
+    if '+' in i:
+        i = i.split(' + ')
+        new_sp1.append(int(i[1]))
+        #print(new_sp1)
+    else:
+        new_sp1.append(int(i))
 print()
-print(new_sp1)
+print("Коэффициенты первого уравнения:", new_sp1)
 
+sp2 = sp2[:-3].split('x^')
+
+for i in sp2:
+    if '+' in i:
+        i = i.split(' + ')
+        new_sp2.append(int(i[1]))
+    else:
+        new_sp2.append(int(i))
+print()
+print("Коэффициенты второго уравнения:", new_sp2)
+
+summa = []
+print()
+summa = list(map(lambda x, y: x + y, new_sp1, new_sp2))
+print('Сумма коэффициентов уравнения:', summa)
+
+formula = ' '
+for i in range(0, len(summa) - 1):
+            if summa[i] != 0:                            #i != len(sp_k) - 1 and sp_k[i] != 0 and i != len(sp_k) - 2:
+                formula += f'{summa[i]}x^{len(summa) - i - 1}'
+                if summa[i + 1] != 0:
+                    formula += ' + '
+formula += f'{summa[-1]} = 0'
+print("Многочлен: ", formula)
+
+with open('Task_5_rezult.txt', 'w') as data:
+    data.write(formula)
